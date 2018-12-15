fdfb313d964441dd6c569402d36501bdd4a41845
NodaRyohei <ryoheinod@gmail.com>
2018-12-16 02:04:48 +0900

    一応うまくいく

--- hatodokei/hatodokei.ino
+++ hatodokei/hatodokei.ino
@@ -13,18 +13,25 @@ int nowsecond;
 int pretime;
 int preminute;
 int presecond;
+int primeCount = 0;
+int ugoku = 0;
+int valServo = 544;
+
+int primeNumber[ ] = {2 , 3, 5, 7, 11,  13,  17,  19,
+23, 29,  31,  37,  41,  43,  47,  53,  59};
 
 void setup() {
   // put your setup code here, to run once:
   lcd.print("hatodokei");
   #if 1
-    setTime(23, 59, 45, 19, 6, 2016);
+    setTime(12, 00, 45, 19, 6, 2016);
   #else
     setTime(86400);
   #endif
 
   Serial.begin(1000000);
   servo.attach(2);
+  servo.write(544);
   delay(2000);
   lcd.clear();
   lcd.setCursor(2,1);
@@ -90,8 +97,29 @@ void loop() {
   pretime = nowtime;
   preminute = nowminute;
   presecond = nowsecond;
+  if(nowminute == primeNumber[primeCount]){
+    ugoku = 1;
+    primeCount+=1;
+  }
+  if(primeCount>17){
+    primeCount=0;
+  }
+  if(ugoku==2){
+    valServo -= 100;
+    servo.write(valServo);
+    if(valServo<544){
+      ugoku = 0;
+    }
+  }
+  if(ugoku==1){
+    valServo += 100;
+    servo.write(valServo);
+    if(valServo>2300){
+      ugoku = 2;
+    }
+  }
   Serial.print(nowminute);
   Serial.print(" ");
   Serial.println(nowsecond);
-  
+  delay(50);
 }
