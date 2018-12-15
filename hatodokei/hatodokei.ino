afe88b799d972fc8b8dddb716dd6f992b12f8917
NodaRyohei <ryoheinod@gmail.com>
2018-12-16 00:56:47 +0900

    delay入れて毎loopごとにクリアしたやつ 結局ダメだった

--- hatodokei/hatodokei.ino
+++ hatodokei/hatodokei.ino
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include<LiquidCrystal.h>
 
-LiquidCrystal lcd(10,9,8,7,6,5,4);
+LiquidCrystal lcd(10, 9, 8, 7, 6, 5, 4);
 
 Servo servo;
 
@@ -17,17 +17,17 @@ int presecond;
 void setup() {
   // put your setup code here, to run once:
   lcd.print("hatodokei");
-  #if 1
-    setTime(23, 59, 45, 19, 6, 2016);
-  #else
-    setTime(86400);
-  #endif
+#if 1
+  setTime(00, 45, 01, 19, 6, 2016);
+#else
+  setTime(86400);
+#endif
 
   Serial.begin(1000000);
   servo.attach(2);
   delay(2000);
   lcd.clear();
-  lcd.setCursor(2,1);
+  lcd.setCursor(2, 1);
   lcd.print(":");
 }
 
@@ -36,62 +36,47 @@ void loop() {
   nowtime = hour();
   nowminute = minute();
   nowsecond = second();
-  if(nowsecond==0){
-      lcd.setCursor(6,1);
-      lcd.print(0);
-      lcd.setCursor(7,1);
-      lcd.print(0);
+  lcd.clear();
+  lcd.setCursor(2, 1);
+  lcd.print(":");
+  if (nowsecond < 10) {
+    lcd.setCursor(6, 1);
+    lcd.print(0);
+    lcd.setCursor(7, 1);
+    lcd.print(nowsecond);
+  } else {
+    lcd.setCursor(6, 1);
+    lcd.print(nowsecond);
   }
-  if(nowsecond > presecond){
-    if(nowsecond<10){
-      lcd.setCursor(6,1);
-      lcd.print(0);
-      lcd.setCursor(7,1);
-      lcd.print(nowsecond);
-    }else{
-      lcd.setCursor(6,1);
-      lcd.print(nowsecond);
-    }
+
+
+  if (nowminute < 10) {
+    lcd.setCursor(3, 1);
+    lcd.print(0);
+    lcd.setCursor(4, 1);
+    lcd.print(nowminute);
+  } else {
+    lcd.setCursor(3, 1);
+    lcd.print(nowminute);
   }
-  if(nowminute==0){
-      lcd.setCursor(3,1);
-      lcd.print(0);
-      lcd.setCursor(4,1);
-      lcd.print(0);
+
+
+  if (nowtime < 10) {
+    lcd.setCursor(0, 1);
+    lcd.print(0);
+    lcd.setCursor(1, 1);
+    lcd.print(nowtime);
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.print(nowtime);
   }
-  if(nowminute > preminute){
-    if(nowminute<10){
-      lcd.setCursor(3,1);
-      lcd.print(0);
-      lcd.setCursor(4,1);
-      lcd.print(nowminute);
-    }else{
-      lcd.setCursor(3,1);
-      lcd.print(nowminute);
-    }
-  }
-  if(nowtime==0){
-      lcd.setCursor(0,1);
-      lcd.print(0);
-      lcd.setCursor(1,1);
-      lcd.print(0);
-  }
-  if(nowtime > pretime){
-    if(nowtime<10){
-      lcd.setCursor(0,1);
-      lcd.print(0);
-      lcd.setCursor(1,1);
-      lcd.print(nowtime);
-    }else{
-      lcd.setCursor(0,1);
-      lcd.print(nowtime);
-    }
-  }
+  
   pretime = nowtime;
   preminute = nowminute;
   presecond = nowsecond;
   Serial.print(nowminute);
   Serial.print(" ");
   Serial.println(nowsecond);
-  
+  delay(50);
+
 }
